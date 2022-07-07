3ded638b7ba53037babae28a0ea4a984b30cd914
nthpcd <108862633+nthpcd@users.noreply.github.com>
2022-07-07 15:19:21 +0200

    close

--- dice_roller.py
+++ dice_roller.py
@@ -6,12 +6,12 @@ def main():
  for i in range(0, dice_rolls) :
    roll = random.randint(1,dice_size)
    dice_sum = dice_sum + roll
-  if roll == 1:
+   if roll == 1:
     print(f'You rolled a {roll}! Critical Fail')
-  elif elif roll == dice_size:
-    print(f'You rolled a {roll}! Critical Success!')
-  else:
-    print(f'You rolled a {roll}')
+    elif elif roll == dice_size:
+     print(f'You rolled a {roll}! Critical Success!')
+     else:
+      print(f'You rolled a {roll}')
  print(f'You have rolled a total of {dice_sum}')
 if __name__== "__main__":
   main()
