b139ca05682777434703d69cc7302f3e7947bfa2
nthpcd <108862633+nthpcd@users.noreply.github.com>
2022-07-07 15:33:13 +0200

    Done

--- dice_roller.py
+++ dice_roller.py
@@ -8,7 +8,7 @@ def main():
    dice_sum = dice_sum + roll
    if roll == 1:
     print(f'You rolled a {roll}! Critical Fail')
-    elif elif roll == dice_size:
+    elif roll == dice_size:
      print(f'You rolled a {roll}! Critical Success!')
      else:
       print(f'You rolled a {roll}')
